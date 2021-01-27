7b08d5c21569a686c063a41aa62f61f7e3ad99ca
jpeizer <github@sent.sh>
2021-01-27 18:16:33 -0500

    Able to delete registry entries

--- cleanApplication_windows.go
+++ cleanApplication_windows.go
@@ -1,14 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/sys/windows/registry"
+	"log"
+)
 
 func cleanApplication(config softwareConfig) {
 	// Deletes relevant registry entries for select software/version
 	for _, property := range config.registry {
-		fmt.Println(property)
+		k, err := registry.OpenKey(registry.CURRENT_USER, property, registry.ALL_ACCESS)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer k.Close()
+
+		names, _ := k.ReadSubKeyNames(50)
+
+		for _, name := range names {
+			_ = registry.DeleteKey(k, name)
+		}
+		_ = registry.DeleteKey(k, "")
 	}
+	// TODO: Check for directory after as a way to verify deletion.
 
 	for _, directory := range config.directories {
 		fmt.Println(directory)
 	}
 }
+
